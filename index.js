54a5fb182c00f0a342b3cfb72ffd6e8e6caf187a
Lucy Wall <lhwall@gmail.com>
2018-11-14 21:17:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  const newArray = array.map(function(item) {return item.toLowerCase() })
+  return newArray
+}
+
+function nameToAttributes(array){
+  const newArray = array.map(function(item) {return {}})
+}
