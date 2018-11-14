f0d5002655101c636c1f09f48880c1ab293ec91b
Lucy Wall <lhwall@gmail.com>
2018-11-14 21:26:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function lowerCaseDrivers(array){
 }
 
 function nameToAttributes(array){
-  const newArray = array.map(function(item) {return {firstName[item.split[0]], lastName[item.split[1]] }})
+  const newArray = array.map(function(item) {return let newObject = {} newObject.firstName = item.split[0], newObject.lastName = item.split[1] }})
   return newArray
 }
